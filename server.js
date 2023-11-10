5849af41e300313ae3c262ff3abcc8aa02cfb925
SandrineB02 <sagath25@gmail.com>
2023-11-10 18:38:31 +0100

    création du fichier server.js ok

---
+++ server.js
@@ -0,0 +1,28 @@
+/** Les modules */
+const express = require('express')
+const cors = require('cors')
+
+/**Connexion de la base de données */
+
+/**Initialisation de l'API */
+const app = express()
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: "Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization"
+}))
+
+app.use(express.json)
+app.use(express.urlencoded({ extended: true }))
+
+/** Routers */
+
+
+/** Démarrage de l'API */
+
+app.listen(11000, () => {
+    console.log("Oh wonderfull your API is ready...")
+})
+
+
