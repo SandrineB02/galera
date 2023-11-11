753b2a1b5a32801b57b7058d65fe8eee254cd5d1
SandrineB02 <sagath25@gmail.com>
2023-11-11 10:24:56 +0100

    création table Formation ok

---
+++ models/m_formation.js
@@ -0,0 +1,33 @@
+/**Import module */
+
+const { DataTypes } = require('sequelize')
+const sequelize = require('../db.config')
+
+/**Modèle Formation */
+module.exports = (sequelize) => {
+    const Formation = sequelize.define('Formation', {
+        id: {
+            type: DataTypes.INTEGER(10),
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nom: {
+            type: DataTypes.STRING(100),
+            defaultValue: '',
+            allowNull: false
+        },
+        debut: {
+            type: DataTypes.INTEGER(10),
+            defaultValue: '',
+            allowNull: false
+        },
+        fin: {
+            type: DataTypes.INTEGER(10),
+            defaultValue: '',
+            allowNull: false
+        },
+
+    })
+
+    return Formation
+}
